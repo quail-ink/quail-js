ba2d1d83f60e39c4cdc02e13b33e44c28bfe229d
Lyric <5h3ll3x@gmail.com>
2024-06-06 20:22:19 +0900

    mod

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { Client } from './core.ts';
 import { AuxiliaClient } from './auxilia.js';
-import utils from './utils.js';
-import error from './lib/error.js';
-import request from './lib/request.js';
-import auth from './lib/auth.js';
-import i18n from './lib/i18n.js';
+import * as util from './lib/util.js';
+import * as error from './lib/error.js';
+import * as request from './lib/request.js';
+import * as auth from './lib/auth.js';
+import * as i18n from './lib/i18n.js';
 
 export {
   Client, AuxiliaClient,
-  utils, error, request, auth, i18n
+  util, error, request, auth, i18n
 };
