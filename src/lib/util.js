4ccaadf2eee0824c9970ecc9466c43b1449e5554
Lyric <5h3ll3x@gmail.com>
2024-06-07 20:42:12 +0900

    add util.getQueryParams

--- src/lib/util.js
+++ src/lib/util.js
@@ -93,9 +93,27 @@ function copyToClipboard(text) {
   }
 }
 
+function getQueryParams() {
+  const params = {};
+  const query = document.location.search.substring(1);
+  const pairs = query.split("&");
+  for (let i = 0; i < pairs.length; i++) {
+    const pos = pairs[i].indexOf("=");
+    if (pos === -1) continue;
+    const name = pairs[i].substring(0, pos);
+    let value = pairs[i].substring(pos + 1);
+    value = decodeURIComponent(value);
+    params[name] = value;
+  }
+
+  return params;
+}
+
+
 export {
   inject,
   wait,
   debounce,
   copyToClipboard,
+  getQueryParams,
 };
