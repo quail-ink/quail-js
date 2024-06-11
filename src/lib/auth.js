bbffc61f6658ada4525534e200a5c97f69f3233b
Lyric <5h3ll3x@gmail.com>
2024-06-11 09:14:36 +0900

    more auth methods

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,14 +5,14 @@ function getAccessTokenFromEnv() {
   if (auth) {
     try {
       const authObj = JSON.parse(auth);
-      token = authObj.access_token || authObj.token;
+      token = authObj.access_token || authObj.token || '';
     } catch (e) {
       token = '';
     }
   }
 
   if (token === '') {
-    token = window._access_token;
+    token = window._access_token || '';
   }
 
   return token;
@@ -35,8 +35,53 @@ function isLogged() {
   return getAccessTokenFromEnv() !== "";
 }
 
+async function loginByExchangingToken(client, epToken) {
+  const resp = await client.exchangeAccessTokenWithEphemeral(epToken);
+  if (resp.access_token) {
+    updateLocalToken(resp.access_token);
+  }
+  return { profile: resp.user, token: resp.access_token };
+}
+
+async function updateEnvToken(token) {
+  const authStr = localStorage.getItem('auth');
+  let authObj = {};
+  if (authStr) {
+    try {
+      authObj = JSON.parse(auth);
+      authObj.access_token = token;
+    } catch (e) {
+      console.warn('Failed to parse auth object, use fallback one', e);
+      authObj = { access_token: token };
+    }
+  }
+  localStorage.setItem('auth', JSON.stringify(authObj));
+}
+
+async function verify(client) {
+  if (isLogged()) {
+    try {
+      const m = await client.getMe()
+      return m;
+    } catch (e) {
+      logout();
+    }
+  }
+  return null;
+}
+
+function logout() {
+  localStorage.removeItem('auth');
+  window.location.reload();
+}
+
+
 export {
   getAccessTokenFromEnv,
   getProfile,
   isLogged,
+  updateEnvToken,
+  loginByExchangingToken,
+  verify,
+  logout,
 };
